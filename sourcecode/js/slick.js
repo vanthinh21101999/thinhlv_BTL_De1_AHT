bad899207717dfc2a19bc478ee330a27d503d647
= <=>
2021-06-03 09:39:14 +0700

    Hoan Thanh

---
+++ sourcecode/js/slick.js
@@ -0,0 +1,35 @@
+console.clear();
+
+$(".slider").slick({
+    // autoplay: true,
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [{
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 1008,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 300,
+            settings: "unslick"
+        }
+
+    ]
+});
